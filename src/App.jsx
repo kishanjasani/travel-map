452579c1d378889af96649f25153daec6ddc3df8
Kishan Jasani <kishanjasani007@yahoo.in>
2024-07-09 00:48:20 +0530

    Add logo, page navigation, Button component and global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 
+import "./index.css";
+
 function App() {
-  return (
-    <>
-      Travel Map
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="product" element={<Product />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+	return (
+		<>
+			Travel Map
+			<BrowserRouter>
+				<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	)
 }
 
 export default App
