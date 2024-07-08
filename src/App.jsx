8fef774dee150c7fa13876caf1b1568d873b3d31
Kishan Jasani <kishanjasani007@yahoo.in>
2024-07-08 14:02:36 +0530

    Add react-router for the SPA App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
-  const count = 'xyz';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 
+function App() {
   return (
     <>
       Travel Map
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+
+export default function Home() {
+    return (
+        <div>
+            <PageNav />
+            <h1>Home</h1>
+        </div>
+    );
+}
