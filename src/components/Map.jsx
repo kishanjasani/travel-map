19f698f83b7e800485af3272b0c18d67d26e28db
Kishan Jasani <kishanjasani007@yahoo.in>
2024-07-09 22:50:58 +0530

    Fetch the data from querystring using react router hook - Set the querystring data using react router hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,17 @@
+import { useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+
 function Map() {
+	const [searchParams, setSearchParam] = useSearchParams();
+
+	const lat = searchParams.get("lat");
+	const lng = searchParams.get("lng");
+
 	return (
-		<h1>Map</h1>
+		<div className={styles.mapContainer}>
+			<h1>Map</h1>
+			<p>{lat} , {lng}</p>
+		</div>
 	)
 }
 export default Map;
