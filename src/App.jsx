ba6ad63ff37adfc75726544b04a4610026074477
Kishan Jasani <kishanjasani007@yahoo.in>
2024-07-09 10:44:58 +0530

    Add nested routs and index route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 import "./index.css";
 
@@ -12,11 +13,17 @@ function App() {
 		<>
 			<BrowserRouter>
 				<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="product" element={<Product />} />
-				<Route path="pricing" element={<Pricing />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/*" element={<PageNotFound />} />
+					<Route index element={<Home />} />
+					<Route path="product" element={<Product />} />
+					<Route path="pricing" element={<Pricing />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="app" element={<AppLayout />}>
+						<Route index element={<p>List of cities</p>} />
+						<Route path="cities" element={<p>List of cities</p>} />
+						<Route path="countries" element={<p>List of countries</p>} />
+						<Route path="form" element={<p>Form</p>} />
+					</Route>
+					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
 		</>
